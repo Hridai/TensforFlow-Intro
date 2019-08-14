91968712c81e7f00f5d94aadaf1dfdb457431948
Hridai Trivedy <hridaitrivedy@gmail.com>
2019-08-14 19:10:35 +0100

    gradient descent

--- tf intro.py
+++ tf intro.py
@@ -58,3 +58,52 @@ with tf.Session() as sess:
 # the y value previously calculated does not live in memory. multiple sessions do not share the
 # the same state. Variable states are stored on the servers, not in sessions.
     
+''' Implementing Batch Gradient Descent '''
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
+
+housing = fetch_california_housing()
+m,n = housing.data.shape
+housing_data_plus_bias = np.c_[np.ones((m,1)),housing.data]
+standardscaler = StandardScaler(with_mean=True,with_std=True)
+standardscaler.fit(housing_data_plus_bias)
+scaled_housing_data_plus_bias = standardscaler.transform(housing_data_plus_bias)
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1,1),dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n+1,1],-1.0,1.0),name='theta')
+y_pred = tf.matmul(X,theta,name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error),name="mse")
+##gradients = tf.gradients(mse, [theta])[0]
+##training_op = tf.assign(theta, theta - learning_rate * gradients)
+### The above two lines can be optimsed by the below:
+#optimizer = tf.train.GradientDescentOptimizer( learning_rate = learning_rate)
+optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print( 'Epoch', epoch, ' MSE = ', mse.eval())
+        sess.run(training_op)
+    
+    best_theta = theta.eval()
+
+
+
+
+
+
+
+
+
+
+
